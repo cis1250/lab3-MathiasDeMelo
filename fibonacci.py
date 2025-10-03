2baba8efde5983f134d5ccbbd1e0cbab8c05ca63
Mathias De Melo <mdemel05@uoguelph.ca>
2025-10-03 12:03:15 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python3
+a = 0
+b = 1
 
+def get_user_input():
+
+  n = int(input("enter the number of terms in the fibonacci sequence"))
+
+  if n > 0:
+    return n
+  else:
+    print("please enter a valid interger")
+
+
+num_terms = get_user_input()
+
+
+for j in range(num_terms):
+  print(a, end=" ")
+  a = b 
+  b = a + b
+
+print()
 # Fibonacci Sequence Exercise
 # TODO: (Read detailed instructions in the Readme file)
 # Prompt the user for the number of terms.
